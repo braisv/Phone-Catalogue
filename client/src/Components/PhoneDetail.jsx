1583be4ee6542c72e813661f531298315f49cc27
braisv <brais.vidal.rodriguez@gmail.com>
2020-02-12 22:00:44 +0100

    responsive detail

--- client/src/Components/PhoneDetail.jsx
+++ client/src/Components/PhoneDetail.jsx
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronCircleUp,
+  faChevronCircleDown
+} from "@fortawesome/free-solid-svg-icons";
 import "./PhoneDetail.scss";
 
 const PhoneDetail = () => {
@@ -12,10 +17,25 @@ const PhoneDetail = () => {
 
   const params = useParams().phoneId;
   const [phone, setPhone] = useState("");
+  const [features, setFeatures] = useState(false);
 
   useEffect(() => {
     setPhone(phones.filter(el => (el.id === +params ? el : null))[0]);
   }, [phone, phones, params]);
+
+  const openFeatures = () => {
+    setFeatures(!features)
+    if (!features) {
+      document.querySelector(".feature-section").style.height = '140px';
+      document.querySelector(".feature-section").style.margin = '10px';
+      document.querySelector(".feature-section").style.opacity = '1';
+    }
+    if (features) {
+      document.querySelector(".feature-section").style.height = '0';
+      document.querySelector(".feature-section").style.margin = '0';
+      document.querySelector(".feature-section").style.opacity = '0';
+    }
+  }
 
   return (
     <div className="phone-detail flex-column">
@@ -28,15 +48,25 @@ const PhoneDetail = () => {
           classNameName="grid-detail"
         />
       </div>
-      <div className="features-phone flex-column">
-        Features:
-        <div className="feature-section flex">
-          <div className="feature">Color: {phone.color}</div>
-          <div className="feature">Price: {phone.price}$</div>
-          <div className="feature">Screen: {phone.screen}</div>
-          <div className="feature">Processor: {phone.processor}</div>
-          <div className="feature">Ram: {phone.ram}</div>
-        </div>
+      <div
+        className="features-phone flex-column"
+        onClick={() => openFeatures()}
+      >
+        Features
+        {!features ? (
+          <FontAwesomeIcon icon={faChevronCircleUp} />
+        ) : (
+          <FontAwesomeIcon icon={faChevronCircleDown} />
+        )}
+    
+          <div className="feature-section flex">
+            <div className="feature">Color: {phone.color}</div>
+            <div className="feature">Price: {phone.price}$</div>
+            <div className="feature">Screen: {phone.screen}</div>
+            <div className="feature">Processor: {phone.processor}</div>
+            <div className="feature">Ram: {phone.ram}</div>
+          </div>
+
       </div>
       <div className="div9 flex">{phone.description}</div>
     </div>
